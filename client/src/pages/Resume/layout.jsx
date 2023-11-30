0efcde7b122534f829229bb4e9765b8c587304ee
g00gol <jackey2870@gmail.com>
2023-11-30 00:08:08 -0500

    init resume pages

---
+++ client/src/pages/Resume/layout.jsx
@@ -0,0 +1,3 @@
+export default function ResumeLayout({ children }) {
+  return <div>{children}</div>;
+}
---
+++ client/src/pages/Resume/page.jsx
@@ -0,0 +1,9 @@
+import ResumeLayout from "./layout";
+
+export default function ResumePage() {
+  return (
+    <ResumeLayout>
+      <h1>Resume</h1>
+    </ResumeLayout>
+  );
+}
